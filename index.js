82abd4b402f88e8555b90805bfa66fcb70e02c3f
Scott Schwab <scottschwab86@gmail.com>
2023-04-04 10:23:24 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+  return blockNumber > 42 ? blockNumber - 42 : 42 - blockNumber;
+}
+
+function distanceFromHqInFeet(blockNumber) {
+  let distanceInFeet = 264 * distanceFromHqInBlocks(blockNumber);
+  return distanceInFeet;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  let numberOfBlocksTraveled = start > destination ? start - destination : destination - start;
+  return numberOfBlocksTraveled * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  let numFeetTraveled = distanceTravelledInFeet(start, destination);
+
+  if (numFeetTraveled <= 400 && numFeetTraveled >= 0) {
+    return 0;
+  } else if (numFeetTraveled <= 2000) {
+    numFeetTraveled -= 400;
+    return numFeetTraveled * 0.02;
+  } else if (numFeetTraveled > 2000 && numFeetTraveled <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
